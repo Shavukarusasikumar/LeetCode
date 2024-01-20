de829287323c498aa18c7ef4403b9e6a6f4663b1
Shavukarusasikumar <98202742+Shavukarusasikumar@users.noreply.github.com>
2024-01-20 22:10:12 +0530

    Time: 96 ms (72.24%) | Memory: 60.9 MB (42.58%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     bool pal(string s,int st,int en){
-    while(st<=en){
-        if(s[st++]!=s[en--]){
-            return false;
+        while(st<=en){
+            if(s[st++]!=s[en--]){
+                return false;
+            }
         }
+        return true;
     }
-    return true;
-}
-void helper(string s,int ind,vector<string>&ds,vector<vector<string>>&ans){
-    if(ind==s.size()){
-        ans.push_back(ds);
-        return;
-    }
-    for(int i=ind;i<s.size();i++){
-        if(pal(s,ind,i)){
-            ds.push_back(s.substr(ind,i-ind+1));
-            helper(s,i+1,ds,ans);
-            ds.pop_back();
+    void helper(string s,int ind,vector<string>&ds,vector<vector<string>>&ans){
+        if(s.size()==ind){
+            ans.push_back(ds);
+            return;
         }
-    }
-}
+        for(int i=ind;i<s.size();i++){
+            if(pal(s,ind,i)){
+                ds.push_back(s.substr(ind,i-ind+1));
+                helper(s,i+1,ds,ans);
+                ds.pop_back();
+                }
+            }
+        }
     vector<vector<string>> partition(string s) {
         int ind=0;
         vector<string> ds;
