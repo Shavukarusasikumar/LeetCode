d2ad02b3edd25385fb49469c005f04d963042696
Shavukarusasikumar <98202742+Shavukarusasikumar@users.noreply.github.com>
2024-01-21 21:28:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (7.53%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-         int ways = 1;
-
-        for (int i = 1; i <= n / 2; i++) {
-            double sum = 1;
-
-            for (int j = i; j < 2 * i; j++) {
-                sum *= (double)(n - j) / (j - i + 1);
-            }
-
-            ways +=sum;
+    int helper(int n,vector<int>&dp){
+        if(n<2){
+            return n;
         }
-
-        return ways;
-   
-    
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=helper(n-1,dp)+helper(n-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+2,-1);
+        return helper(n+1,dp);
     }
 };
