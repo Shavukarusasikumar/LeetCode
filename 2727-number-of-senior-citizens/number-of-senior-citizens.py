802b132b183bc72bd3673fd760e320620e52d1ac
Shavukarusasikumar <98202742+Shavukarusasikumar@users.noreply.github.com>
2024-08-01 08:58:21 +0530

    Time: 54 ms (11.67%) | Memory: 16.5 MB (75.00%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.py
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.py
@@ -2,6 +2,6 @@ class Solution:
     def countSeniors(self, details: List[str]) -> int:
         c=0
         for i in details:
-            if i[-4]+i[-3]>'60':
+            if int(i[-4]+i[-3])>60:
                 c+=1
         return c
