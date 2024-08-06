180a17d764e3e710a1bfd7f8a1dfe58a7df8a173
Shavukarusasikumar <98202742+Shavukarusasikumar@users.noreply.github.com>
2024-08-06 13:05:33 +0530

    Time: 40 ms (28.46%) | Memory: 16.6 MB (23.74%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        d=Counter(word)
+        d=sorted(d.values(),reverse=True)
+        c = 0
+        bc = 0
+        for i in d:
+            c += 1
+            if c < 9:
+                bc += i
+            elif c < 17:
+                bc += (2 * i)
+            elif c<25:
+                bc += (3 * i)
+            else:
+                bc+=(4*i)
+        return bc
